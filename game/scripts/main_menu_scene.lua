6c4503708cf839c4efdd07b435a567b5a10f37b8
Cris Jr <crisjr@pm.me>
2023-11-11 09:34:51 -0300

    Draw main menu

--- game/scripts/main_menu_scene.lua
+++ game/scripts/main_menu_scene.lua
@@ -16,7 +16,14 @@ main_menu_scene.update = function(context)
 end
 
 main_menu_scene.draw = function(context)
+  local high_score = 0  -- TODO implement high score!
+
   gfx.clear()
   gfx.drawText("PLEASE GIVE ME A NAME", 50, 50)
+  gfx.drawText("- Use the dock to move up and down", 50, 70)
+  gfx.drawText("- Press any button to move right", 50, 90)
+  gfx.drawText("- Surpass as many barriers as possible in 60s", 50, 110)
+  gfx.drawText("Press any button to start and good luck!", 50, 150)
+  gfx.drawText("HIGH SCORE: " .. high_score, 50, 170)
 end
 
