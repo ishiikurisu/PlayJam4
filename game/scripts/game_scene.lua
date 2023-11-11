1ca2053428f53c261ca4e356136af4693e616ee9
Cris Jr <crisjr@pm.me>
2023-11-11 09:53:32 -0300

    Draw basic player character

--- game/scripts/game_scene.lua
+++ game/scripts/game_scene.lua
@@ -2,9 +2,18 @@ game_scene = {}
 local pd <const> = playdate
 local gfx <const> = playdate.graphics
 
+local PLAYER_WIDTH <const> = 8
+local PLAYER_HEIGHT <const> = 8
+
 game_scene.setup = function(context, init_params)
   UPDATE = game_scene.update
   DRAW = game_scene.draw
+  context = {
+    state = "start",
+    player = {
+      pos_y = playdate.display.getHeight()/2 - PLAYER_HEIGHT/2,
+    },
+  }
   return context
 end
 
@@ -14,6 +23,11 @@ end
 
 game_scene.draw = function(context)
   gfx.clear()
-  gfx.drawText("and now what?", 50, 50)
+
+  -- drawing player character
+  local player = context.player
+  local pos_x = 10
+  local pos_y = player.pos_y
+  gfx.fillRoundRect(pos_x, pos_y, PLAYER_WIDTH, PLAYER_HEIGHT, 2)
 end
 
