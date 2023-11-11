dd3c7e8e7df885dbc7b11513bf568aa606845f65
Cris Jr <crisjr@pm.me>
2023-11-11 10:07:34 -0300

    move player character up and down

--- game/scripts/game_scene.lua
+++ game/scripts/game_scene.lua
@@ -4,6 +4,41 @@ local gfx <const> = playdate.graphics
 
 local PLAYER_WIDTH <const> = 8
 local PLAYER_HEIGHT <const> = 8
+local SCREEN_HEIGHT <const> = pd.display.getHeight()
+
+-- #######################
+-- # AUXILIARY FUNCTIONS #
+-- #######################
+
+local minmax = function(x, floor, ceil)
+  local y = x
+  if y > ceil then
+    y = ceil
+  end
+  if y < floor then
+    y = floor
+  end
+  return y
+end
+
+local degree_change_to_cartesian_change = function(dg)
+  -- screen_size / 360 degrees = screen_dx / delta_degrees
+  -- therefore screen_dx = delta_degress * screen_size / 360 degrees
+  return dg * pd.display.getHeight() / 360
+end
+
+local handle_game_running_input = function(context)
+  local player = context.player
+  local change, accelerated_change = pd.getCrankChange()
+  local delta_y = degree_change_to_cartesian_change(change)
+  player.pos_y = minmax(player.pos_y + delta_y, 0, SCREEN_HEIGHT - PLAYER_HEIGHT)
+  return context
+end
+
+
+-- ###################
+-- # MAIN OPERATIONS #
+-- ###################
 
 game_scene.setup = function(context, init_params)
   UPDATE = game_scene.update
@@ -11,13 +46,14 @@ game_scene.setup = function(context, init_params)
   context = {
     state = "start",
     player = {
-      pos_y = playdate.display.getHeight()/2 - PLAYER_HEIGHT/2,
+      pos_y = SCREEN_HEIGHT/2 - PLAYER_HEIGHT/2,
     },
   }
   return context
 end
 
 game_scene.update = function(context)
+  context = handle_game_running_input(context)
   return context
 end
 
